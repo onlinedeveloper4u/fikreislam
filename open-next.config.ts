926403635c87dd3d13b66a99b87ca26cbfcc78f1
Muhammad Aqib Rafiqe <71644910+onlinedeveloper4u@users.noreply.github.com>
2026-03-26 23:44:00 +0500

    feat: Configure OpenNext to include `node:crypto` as an edge external and enable external middleware.

--- open-next.config.ts
+++ open-next.config.ts
@@ -4,6 +4,10 @@ const config: OpenNextConfig = {
   default: {
     runtime: "edge",
   },
+  edgeExternals: ["node:crypto"],
+  middleware: {
+    external: true,
+  },
 };
 
 export default config;
